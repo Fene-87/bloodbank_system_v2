37396a1924eabe6429167a900872a11e746c1b56
Fene-87 <Fenekayas87@gmail.com>
2023-11-25 00:37:39 +0300

    Fix navbar styling

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,8 +1,15 @@
 import { useParams, NavLink } from 'react-router-dom';
 import './navbar.css'
+import { useState } from 'react';
 
 
 const Navbar = ({ type }) => {
+    const [activeLink, setActiveLink] = useState(1);
+
+    const handleClick = (index) => {
+      setActiveLink(index);
+    }
+
     return (
         <div className="navbar-container">
             <ul className='navbar-items'>
@@ -15,24 +22,24 @@ const Navbar = ({ type }) => {
                   :
                   <>
                     <div className='navbar-links-section'>
-                      <NavLink to='/' className="navbar-link">
-                        <li className='link-text'>Home</li>
+                      <NavLink to='/' onClick={() => handleClick(1)} className="navbar-link">
+                        <li className={activeLink === 1 ? 'active-link' : 'link-text'}>Home</li>
                       </NavLink>
 
-                      <NavLink to='/' className="navbar-link">
-                        <li className='link-text'>About Us</li>
+                      <NavLink to='/' onClick={() => handleClick(2)} className="navbar-link">
+                        <li className={activeLink === 2 ? 'active-link' : 'link-text'}>About Us</li>
                       </NavLink>
 
-                      <NavLink to='/' className="navbar-link">
-                        <li className='link-text'>Services</li>
+                      <NavLink to='/' onClick={() => handleClick(3)} className="navbar-link">
+                        <li className={activeLink === 3 ? 'active-link' : 'link-text'}>Services</li>
                       </NavLink>
 
-                      <NavLink to='/donate' className="navbar-link">
-                        <li className='link-text'>Donate blood</li>
+                      <NavLink to='/donate' onClick={() => handleClick(4)} className="navbar-link">
+                        <li className={activeLink === 4 ? 'active-link' : 'link-text'}>Donate blood</li>
                       </NavLink>
 
-                      <NavLink to='/recipient' className="navbar-link">
-                        <li className='link-text'>Recipients</li>
+                      <NavLink to='/recipient' onClick={() => handleClick(5)} className="navbar-link">
+                        <li className={activeLink === 5 ? 'active-link' : 'link-text'}>Recipients</li>
                       </NavLink>
                     </div>
                     <NavLink to='/admin' className="navbar-link">
