dadbfed55f983fe04909639f2e04d76758f2b7d1
Fene-87 <fenekayas87@gmail.com>
2024-02-05 12:09:37 +0300

    Create User model

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mysql2 from "mysql2";
 import cors from "cors";
 import dotenv from "dotenv";
-// import authRoute from "./routes/auth.js";
+import authRoute from "./routes/auth.js";
 // import donationRoute from "./routes/donation.js";
 // import adminRoute from "./routes/admin.js";
 import mongoose from "mongoose";
@@ -13,7 +13,7 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-// app.use('/server/auth', authRoute);
+app.use('/server/auth', authRoute);
 // app.use('/server/donation', donationRoute);
 // app.use('/server/admin', adminRoute);
 
---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nationalID: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bloodType: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+})
